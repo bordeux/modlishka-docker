fb432cf35ae16eab9a4812fa7236acdf24efaa52
Chris Bednarczyk <chris@tourradar.com>
2019-01-18 17:08:02 +0100

    fix

--- run-server.sh
+++ run-server.sh
@@ -10,7 +10,7 @@ do
         envName=$(echo "${line}" | cut -d'=' -f 1 | tr '[:upper:]' '[:lower:]')
         if [[ "${envName:0:3}" = "ml_" ]] ;
         then
-			optionName=$(echo "${envName}" | sed -r 's/(^ml\_)//g' | sed -r 's/(_)([a-z])/\U\2/g')
+			optionName=$(echo "${envName}" | sed -r 's/(^ml\_)//g' |awk -F'_' '{ printf $1; for(i=2; i<=NF; i++) printf toupper(substr($i,1,1)) substr($i,2);printf "\n"}')
 			optionValue=$(echo "${line}" | sed -r 's/(^.*)\=//g')
 			RUN_CMD="${RUN_CMD} -${optionName} ${optionValue}"
         fi
