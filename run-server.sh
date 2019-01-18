b91c75bf83eb8be489449ab2b346bfd37164355e
Chris Bednarczyk <chris@tourradar.com>
2019-01-18 16:13:08 +0100

    Fix run server

--- run-server.sh
+++ run-server.sh
@@ -2,15 +2,19 @@
 set -e
 
 RUN_CMD='Modlishka'
-env | while IFS='=' read -r -d '' envName envValue; do
-	envName=$(echo "${envName}" | tr '[:upper:]' '[:lower:]')
-	if [[ ${envName} == ml* ]] ;
-	then
-		OPTION_NAME=$(echo "${envName}" | sed -r 's/(^ml\_)//g' | sed -r 's/(_)([a-z])/\U\2/g')
-		RUN_CMD="${RUN_CMD} -${OPTION_NAME} ${envValue}"
-	fi
+IFS='
+'
+envList=$(env)
+for line in $envList
+do
+        envName=$(echo "${line}" | cut -d'=' -f 1 | tr '[:upper:]' '[:lower:]')
+        if [[ "${envName:0:3}" = "ml_" ]] ;
+        then
+			optionName=$(echo "${envName}" | sed -r 's/(^ml\_)//g' | sed -r 's/(_)([a-z])/\U\2/g')
+			RUN_CMD="${RUN_CMD} -${optionName} ${optionName}"
+        fi
 done
 
 echo "Running command: ${RUN_CMD}"
 
-${RUN_CMD}
+#${RUN_CMD}
